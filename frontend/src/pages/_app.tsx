4b6b09f321d3615593e40b046a901f6e47f92145
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-08 23:07:03 +0900

    :wrench: Introduce Chakra UI (#30)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import '../../styles/globals.css';
 
 /* eslint-disable react/jsx-props-no-spreading */
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
