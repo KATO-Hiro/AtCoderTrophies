87d3c71e284a18dbede077785c04b9ef4c427bf9
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-15 11:36:08 +0000

    :boom: Remove chakura ui (#53)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
 
 import '../../styles/globals.css';
 
 /* eslint-disable react/jsx-props-no-spreading */
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
