de3c3a4765080d76e20d369efc02a2aca7b484ad
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-18 05:52:47 +0000

    Refactoring (#63)

--- frontend/src/components/Copyright/Copyright.tsx
+++ frontend/src/components/Copyright/Copyright.tsx
@@ -1,20 +1,20 @@
 import { Link, Typography } from '@material-ui/core';
 
-import { MY_ATCODER_PAGE } from '../../constants/urls';
+import { ATCODER_USER_NAME, MY_ATCODER_PAGE } from '../../constants/urls';
 
 // See:
 // https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/templates/dashboard
 export default function Copyright(): JSX.Element {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
-      Copyright ©{new Date().getFullYear()}{' '}
+      Copyright © {new Date().getFullYear()}{' '}
       <Link
         color='inherit'
         href={MY_ATCODER_PAGE}
         target='_blank'
         rel='noreferrer'
       >
-        hiro_hiro
+        {ATCODER_USER_NAME}
       </Link>
       , All Rights Reserved.
     </Typography>
