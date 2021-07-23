e9e9ec9f55e701e58c6903cfa0d9740f19e39da1
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-23 08:58:55 +0000

    :art: Add preview component (#90)

---
+++ frontend/src/parts/TrophyCabinet/Preview.tsx
@@ -0,0 +1,7 @@
+const Preview = (): JSX.Element => (
+  <>
+    <h2>Preview</h2>
+  </>
+);
+
+export default Preview;
--- frontend/src/parts/TrophyCabinet/TrophyCabinet.tsx
+++ frontend/src/parts/TrophyCabinet/TrophyCabinet.tsx
@@ -1,6 +1,10 @@
+import Preview from './Preview';
+
 const TrophyCabinet = (): JSX.Element => (
   <>
     {/* <Preview /> */}
+    <Preview />
+
     {/* <TwitterShareButton /> */}
     {/* <HtmlCopyField /> */}
     {/* <MarkdownCopyField /> */}
