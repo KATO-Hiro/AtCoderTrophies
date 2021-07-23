c3ba0d210e64800ccd9bd261aed4bc7d20f7f207
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-23 08:45:35 +0000

    :bug: Add return type (#90)

--- frontend/src/components/ListItemLink/ListItemLink.tsx
+++ frontend/src/components/ListItemLink/ListItemLink.tsx
@@ -2,7 +2,7 @@ import { ListItem } from '@material-ui/core';
 
 // See:
 // https://github.com/anasyusef/Society-app-react-rails/blob/8cc416446392411f4598f5ed1484911f7a7693ec/app/javascript/components/Dashboard/Dashboard.jsx
-function ListItemLink(props: any) {
+function ListItemLink(props: any): JSX.Element {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <ListItem button component='a' {...props} />;
 }
