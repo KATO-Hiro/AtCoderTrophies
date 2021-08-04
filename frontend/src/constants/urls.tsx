6f2ca2938b9a00d307b6acef0802da58258a5160
Kato Hiroki <k.hiro1818@gmail.com>
2021-08-04 13:22:14 +0000

    :bug: Add type (#137)

--- frontend/src/constants/urls.tsx
+++ frontend/src/constants/urls.tsx
@@ -6,7 +6,7 @@ export const MY_ATCODER_PAGE = `${ATCODER_BASE_URL}/users/${ATCODER_USER_NAME}`;
 
 export const GITHUB_URL = 'https://github.com/KATO-Hiro/AtCoderTrophies';
 
-export const TWITTER_USER_NAME = '@k_hiro1818';
+export const TWITTER_USER_NAME = '@k_hiro1818' as string;
 
 export const TWITTER_URL = 'https://twitter.com/k_hiro1818';
 
