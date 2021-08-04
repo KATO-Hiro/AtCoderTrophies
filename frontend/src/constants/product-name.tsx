544a175dcf825e5424b31248fcba4bb56573734c
Kato Hiroki <k.hiro1818@gmail.com>
2021-08-04 13:08:54 +0000

    :books: Add meta for OGP (#54)

--- frontend/src/constants/product-name.tsx
+++ frontend/src/constants/product-name.tsx
@@ -1,5 +1,7 @@
 const PRODUCT_NAME = 'AtCoder Trophies';
 
+export const PRODUCT_URL = 'https://atcoder-trophies.vercel.app';
+
 export const PRODUCT_DESCRIPTION =
   'Show dynamically generated AtCoder Stat Trophies.';
 
