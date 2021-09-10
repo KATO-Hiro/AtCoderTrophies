9ea31efc6043a92bc80a570c8d0b72b96875f4ca
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-10 13:31:45 +0000

    💥 Revise the required score downward (#337)

--- frontend/src/components/Trophy/BashUserTrophy.tsx
+++ frontend/src/components/Trophy/BashUserTrophy.tsx
@@ -5,12 +5,12 @@ import Trophy from './Trophy';
 export default class BashUserTrophy extends Trophy {
   constructor(score: number) {
     const rankConditions = [
-      new RankCondition(RANK.SSS, 'God Solver', 5000),
-      new RankCondition(RANK.SS, 'Legendary Solver', 2500),
-      new RankCondition(RANK.S, 'Ultra Solver', 1000),
-      new RankCondition(RANK.AAA, 'Hyper Solver', 500),
-      new RankCondition(RANK.AA, 'Super Solver', 250),
-      new RankCondition(RANK.A, 'Regular Solver', 100),
+      new RankCondition(RANK.SSS, 'God Solver', 1000),
+      new RankCondition(RANK.SS, 'Legendary Solver', 500),
+      new RankCondition(RANK.S, 'Ultra Solver', 250),
+      new RankCondition(RANK.AAA, 'Hyper Solver', 100),
+      new RankCondition(RANK.AA, 'Super Solver', 50),
+      new RankCondition(RANK.A, 'Regular Solver', 25),
       new RankCondition(RANK.B, 'Beginner Solver', 10),
       new RankCondition(RANK.C, 'First AC', 1),
     ];
