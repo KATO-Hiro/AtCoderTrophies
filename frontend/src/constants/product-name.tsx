8e94210401d4827a80a843747253125d4e631eaf
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-15 05:29:34 +0000

    :wastebasket: Remove old API and fetcher (#350)

--- frontend/src/constants/product-name.tsx
+++ frontend/src/constants/product-name.tsx
@@ -9,18 +9,6 @@ export const BACKEND_BASE_URL = 'https://atcoder-trophies-api.vercel.app';
 
 export const BACKEND_API_V1 = '/v1';
 
-export const ACCEPTED_COUNT_API_URL = (userName: string): string =>
-  `${BACKEND_BASE_URL + BACKEND_API_V1}/ac_count/${userName}`;
-
-export const ACCEPTED_COUNT_BY_LANGUAGE_API_URL = (userName: string): string =>
-  `${BACKEND_BASE_URL + BACKEND_API_V1}/ac_count_by_lang/${userName}`;
-
-export const RATED_POINT_SUM_API_URL = (userName: string): string =>
-  `${BACKEND_BASE_URL + BACKEND_API_V1}/rated_point_sum/${userName}`;
-
-export const LONGEST_STREAK_API_URL = (userName: string): string =>
-  `${BACKEND_BASE_URL + BACKEND_API_V1}/longest_streak/${userName}`;
-
 export const ATCODER_PROBLEMS_STAT_API_URL = (userName: string): string =>
   `${BACKEND_BASE_URL + BACKEND_API_V1}/problems_stat_api/${userName}`;
 
