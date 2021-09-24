aeac6127431090970eb21cffdba5352e59a3b9c6
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-24 14:02:47 +0000

    :recycle: Fix format (#425)

--- frontend/src/components/TrophyList/TrophyList.tsx
+++ frontend/src/components/TrophyList/TrophyList.tsx
@@ -1,48 +1,48 @@
-import { SupportedLanguages as Languages } from '../../constants/languages';
-import { RANK, RANK_ORDER } from '../../constants/rank';
-import { SupportedLanguages } from '../../interfaces/SupportedLanguages';
-import AcceptedCountTrophy from '../Trophy/AcceptedCountTrophy';
-import AdaUserTrophy from '../Trophy/AdaUserTrophy';
-import AllSuperRankTrophy from '../Trophy/AllSuperRankTrophy';
-import AwkUserTrophy from '../Trophy/AwkUserTrophy';
-import BashUserTrophy from '../Trophy/BashUserTrophy';
-import BcUserTrophy from '../Trophy/BcUserTrophy';
-import BrainfuckUserTrophy from '../Trophy/BrainfuckUserTrophy';
-import CPlusPluserTrophy from '../Trophy/CPlusPluserTrophy';
-import CProgrammerTrophy from '../Trophy/CProgrammerTrophy';
-import CSharperTrophy from '../Trophy/CSharperTrophy';
-import CeylonUserTrophy from '../Trophy/CeylonUserTrophy';
-import ClojureUserTrophy from '../Trophy/ClojureUserTrophy';
-import CommonLisperTrophy from '../Trophy/CommonLisperTrophy';
-import CrystalUserTrophy from '../Trophy/CrystalUserTrophy';
-import CythonUserTrophy from '../Trophy/CythonUserTrophy';
-import DProgrammerTrophy from '../Trophy/DProgrammerTrophy';
-import DartUserTrophy from '../Trophy/DartUserTrophy';
-import DashUserTrophy from '../Trophy/DashUserTrophy';
-import ElixirUserTrophy from '../Trophy/ElixirUserTrophy';
-import FortranUserTrophy from '../Trophy/FortranUserTrophy';
-import GopherTrophy from '../Trophy/GopherTrophy';
-import HaskellerTrophy from '../Trophy/HaskellerTrophy';
-import JavaScripterTrophy from '../Trophy/JavaScripterTrophy';
-import JavaerTrophy from '../Trophy/JavaerTrophy';
-import JuliaUserTrophy from '../Trophy/JuliaUserTrophy';
-import KotlinerTrophy from '../Trophy/KotlinerTrophy';
-import LongestStreakCountTrophy from '../Trophy/LongestStreakCountTrophy';
-import LuaUserTrophy from '../Trophy/LuaUserTrophy';
-import NimUserTrophy from '../Trophy/NimUserTrophy';
-import OCamelUserTrophy from '../Trophy/OCamelUserTrophy';
-import PHPerTrophy from '../Trophy/PHPerTrophy';
-import PascalUserTrophy from '../Trophy/PascalUserTrophy';
-import PerlerTrophy from '../Trophy/PerlerTrophy';
-import PyPyUserTrophy from '../Trophy/PyPyUserTrophy';
-import PythonistaTrophy from '../Trophy/PythonistaTrophy';
-import RatedPointSumTrophy from '../Trophy/RatedPointSumTrophy';
-import RubyistTrophy from '../Trophy/RubyistTrophy';
-import RustaceanTrophy from '../Trophy/RustaceanTrophy';
-import SwiftUserTrophy from '../Trophy/SwiftUserTrophy';
-import Trophy from '../Trophy/Trophy';
-import VimmerTrophy from '../Trophy/VimmerTrophy'
-import UserInfo from '../UserInfo/UserInfo';
+import { SupportedLanguages as Languages } from "../../constants/languages";
+import { RANK, RANK_ORDER } from "../../constants/rank";
+import { SupportedLanguages } from "../../interfaces/SupportedLanguages";
+import AcceptedCountTrophy from "../Trophy/AcceptedCountTrophy";
+import AdaUserTrophy from "../Trophy/AdaUserTrophy";
+import AllSuperRankTrophy from "../Trophy/AllSuperRankTrophy";
+import AwkUserTrophy from "../Trophy/AwkUserTrophy";
+import BashUserTrophy from "../Trophy/BashUserTrophy";
+import BcUserTrophy from "../Trophy/BcUserTrophy";
+import BrainfuckUserTrophy from "../Trophy/BrainfuckUserTrophy";
+import CPlusPluserTrophy from "../Trophy/CPlusPluserTrophy";
+import CProgrammerTrophy from "../Trophy/CProgrammerTrophy";
+import CSharperTrophy from "../Trophy/CSharperTrophy";
+import CeylonUserTrophy from "../Trophy/CeylonUserTrophy";
+import ClojureUserTrophy from "../Trophy/ClojureUserTrophy";
+import CommonLisperTrophy from "../Trophy/CommonLisperTrophy";
+import CrystalUserTrophy from "../Trophy/CrystalUserTrophy";
+import CythonUserTrophy from "../Trophy/CythonUserTrophy";
+import DProgrammerTrophy from "../Trophy/DProgrammerTrophy";
+import DartUserTrophy from "../Trophy/DartUserTrophy";
+import DashUserTrophy from "../Trophy/DashUserTrophy";
+import ElixirUserTrophy from "../Trophy/ElixirUserTrophy";
+import FortranUserTrophy from "../Trophy/FortranUserTrophy";
+import GopherTrophy from "../Trophy/GopherTrophy";
+import HaskellerTrophy from "../Trophy/HaskellerTrophy";
+import JavaScripterTrophy from "../Trophy/JavaScripterTrophy";
+import JavaerTrophy from "../Trophy/JavaerTrophy";
+import JuliaUserTrophy from "../Trophy/JuliaUserTrophy";
+import KotlinerTrophy from "../Trophy/KotlinerTrophy";
+import LongestStreakCountTrophy from "../Trophy/LongestStreakCountTrophy";
+import LuaUserTrophy from "../Trophy/LuaUserTrophy";
+import NimUserTrophy from "../Trophy/NimUserTrophy";
+import OCamelUserTrophy from "../Trophy/OCamelUserTrophy";
+import PHPerTrophy from "../Trophy/PHPerTrophy";
+import PascalUserTrophy from "../Trophy/PascalUserTrophy";
+import PerlerTrophy from "../Trophy/PerlerTrophy";
+import PyPyUserTrophy from "../Trophy/PyPyUserTrophy";
+import PythonistaTrophy from "../Trophy/PythonistaTrophy";
+import RatedPointSumTrophy from "../Trophy/RatedPointSumTrophy";
+import RubyistTrophy from "../Trophy/RubyistTrophy";
+import RustaceanTrophy from "../Trophy/RustaceanTrophy";
+import SwiftUserTrophy from "../Trophy/SwiftUserTrophy";
+import Trophy from "../Trophy/Trophy";
+import VimmerTrophy from "../Trophy/VimmerTrophy";
+import UserInfo from "../UserInfo/UserInfo";
 
 export default class TrophyList {
   private trophies = new Array<Trophy>();
@@ -61,123 +61,123 @@ export default class TrophyList {
     // TODO: Refactoring.
     this.addTrophyAsNeeded(new AcceptedCountTrophy(acceptedCount));
     this.addTrophyAsNeeded(
-      new AdaUserTrophy(this.getAcceptedCountByLanguage(Languages.ADA)),
+      new AdaUserTrophy(this.getAcceptedCountByLanguage(Languages.ADA))
     );
     this.addTrophyAsNeeded(
-      new AwkUserTrophy(this.getAcceptedCountByLanguage(Languages.AWK)),
+      new AwkUserTrophy(this.getAcceptedCountByLanguage(Languages.AWK))
     );
     this.addTrophyAsNeeded(
-      new BashUserTrophy(this.getAcceptedCountByLanguage(Languages.BASH)),
+      new BashUserTrophy(this.getAcceptedCountByLanguage(Languages.BASH))
     );
     this.addTrophyAsNeeded(
-      new BcUserTrophy(this.getAcceptedCountByLanguage(Languages.BC)),
+      new BcUserTrophy(this.getAcceptedCountByLanguage(Languages.BC))
     );
     this.addTrophyAsNeeded(
       new BrainfuckUserTrophy(
-        this.getAcceptedCountByLanguage(Languages.BRAINFUCK),
-      ),
+        this.getAcceptedCountByLanguage(Languages.BRAINFUCK)
+      )
     );
     this.addTrophyAsNeeded(
       new CPlusPluserTrophy(
-        this.getAcceptedCountByLanguage(Languages.C_PLUS_PLUS),
-      ),
+        this.getAcceptedCountByLanguage(Languages.C_PLUS_PLUS)
+      )
     );
     this.addTrophyAsNeeded(
-      new CProgrammerTrophy(this.getAcceptedCountByLanguage(Languages.C)),
+      new CProgrammerTrophy(this.getAcceptedCountByLanguage(Languages.C))
     );
     this.addTrophyAsNeeded(
-      new CSharperTrophy(this.getAcceptedCountByLanguage(Languages.C_SHARP)),
+      new CSharperTrophy(this.getAcceptedCountByLanguage(Languages.C_SHARP))
     );
     this.addTrophyAsNeeded(
-      new CeylonUserTrophy(this.getAcceptedCountByLanguage(Languages.CEYLON)),
+      new CeylonUserTrophy(this.getAcceptedCountByLanguage(Languages.CEYLON))
     );
     this.addTrophyAsNeeded(
-      new ClojureUserTrophy(this.getAcceptedCountByLanguage(Languages.CLOJURE)),
+      new ClojureUserTrophy(this.getAcceptedCountByLanguage(Languages.CLOJURE))
     );
     this.addTrophyAsNeeded(
       new CommonLisperTrophy(
-        this.getAcceptedCountByLanguage(Languages.COMMON_LISP),
-      ),
+        this.getAcceptedCountByLanguage(Languages.COMMON_LISP)
+      )
     );
     this.addTrophyAsNeeded(
-      new CrystalUserTrophy(this.getAcceptedCountByLanguage(Languages.CRYSTAL)),
+      new CrystalUserTrophy(this.getAcceptedCountByLanguage(Languages.CRYSTAL))
     );
     this.addTrophyAsNeeded(
-      new CythonUserTrophy(this.getAcceptedCountByLanguage(Languages.CYTHON)),
+      new CythonUserTrophy(this.getAcceptedCountByLanguage(Languages.CYTHON))
     );
     this.addTrophyAsNeeded(
-      new DProgrammerTrophy(this.getAcceptedCountByLanguage(Languages.D)),
+      new DProgrammerTrophy(this.getAcceptedCountByLanguage(Languages.D))
     );
     this.addTrophyAsNeeded(
-      new DartUserTrophy(this.getAcceptedCountByLanguage(Languages.DART)),
+      new DartUserTrophy(this.getAcceptedCountByLanguage(Languages.DART))
     );
     this.addTrophyAsNeeded(
-      new DashUserTrophy(this.getAcceptedCountByLanguage(Languages.DASH)),
+      new DashUserTrophy(this.getAcceptedCountByLanguage(Languages.DASH))
     );
     this.addTrophyAsNeeded(
-      new ElixirUserTrophy(this.getAcceptedCountByLanguage(Languages.ELIXIR)),
+      new ElixirUserTrophy(this.getAcceptedCountByLanguage(Languages.ELIXIR))
     );
     this.addTrophyAsNeeded(
-      new FortranUserTrophy(this.getAcceptedCountByLanguage(Languages.FORTRAN)),
+      new FortranUserTrophy(this.getAcceptedCountByLanguage(Languages.FORTRAN))
     );
     this.addTrophyAsNeeded(
-      new GopherTrophy(this.getAcceptedCountByLanguage(Languages.GO)),
+      new GopherTrophy(this.getAcceptedCountByLanguage(Languages.GO))
     );
     this.addTrophyAsNeeded(
-      new HaskellerTrophy(this.getAcceptedCountByLanguage(Languages.HASKELL)),
+      new HaskellerTrophy(this.getAcceptedCountByLanguage(Languages.HASKELL))
     );
     this.addTrophyAsNeeded(
       new JavaScripterTrophy(
-        this.getAcceptedCountByLanguage(Languages.JAVASCRIPT),
-      ),
+        this.getAcceptedCountByLanguage(Languages.JAVASCRIPT)
+      )
     );
     this.addTrophyAsNeeded(
-      new JavaerTrophy(this.getAcceptedCountByLanguage(Languages.JAVA)),
+      new JavaerTrophy(this.getAcceptedCountByLanguage(Languages.JAVA))
     );
     this.addTrophyAsNeeded(
-      new JuliaUserTrophy(this.getAcceptedCountByLanguage(Languages.JULIA)),
+      new JuliaUserTrophy(this.getAcceptedCountByLanguage(Languages.JULIA))
     );
     this.addTrophyAsNeeded(
-      new KotlinerTrophy(this.getAcceptedCountByLanguage(Languages.KOTLIN)),
+      new KotlinerTrophy(this.getAcceptedCountByLanguage(Languages.KOTLIN))
     );
     this.addTrophyAsNeeded(new LongestStreakCountTrophy(longestStreak));
     this.addTrophyAsNeeded(
-      new LuaUserTrophy(this.getAcceptedCountByLanguage(Languages.LUA)),
+      new LuaUserTrophy(this.getAcceptedCountByLanguage(Languages.LUA))
     );
     this.addTrophyAsNeeded(
-      new NimUserTrophy(this.getAcceptedCountByLanguage(Languages.NIM)),
+      new NimUserTrophy(this.getAcceptedCountByLanguage(Languages.NIM))
     );
     this.addTrophyAsNeeded(
-      new OCamelUserTrophy(this.getAcceptedCountByLanguage(Languages.OCAML)),
+      new OCamelUserTrophy(this.getAcceptedCountByLanguage(Languages.OCAML))
     );
     this.addTrophyAsNeeded(
-      new PHPerTrophy(this.getAcceptedCountByLanguage(Languages.PHP)),
+      new PHPerTrophy(this.getAcceptedCountByLanguage(Languages.PHP))
     );
     this.addTrophyAsNeeded(
-      new PascalUserTrophy(this.getAcceptedCountByLanguage(Languages.PASCAL)),
+      new PascalUserTrophy(this.getAcceptedCountByLanguage(Languages.PASCAL))
     );
     this.addTrophyAsNeeded(
-      new PerlerTrophy(this.getAcceptedCountByLanguage(Languages.PERL)),
+      new PerlerTrophy(this.getAcceptedCountByLanguage(Languages.PERL))
     );
     this.addTrophyAsNeeded(
-      new PyPyUserTrophy(this.getAcceptedCountByLanguage(Languages.PYPY)),
+      new PyPyUserTrophy(this.getAcceptedCountByLanguage(Languages.PYPY))
     );
     this.addTrophyAsNeeded(
-      new PythonistaTrophy(this.getAcceptedCountByLanguage(Languages.PYTHON)),
+      new PythonistaTrophy(this.getAcceptedCountByLanguage(Languages.PYTHON))
     );
     this.addTrophyAsNeeded(new RatedPointSumTrophy(ratedPointSum));
     this.addTrophyAsNeeded(
-      new RubyistTrophy(this.getAcceptedCountByLanguage(Languages.RUBY)),
+      new RubyistTrophy(this.getAcceptedCountByLanguage(Languages.RUBY))
     );
     this.addTrophyAsNeeded(
-      new RustaceanTrophy(this.getAcceptedCountByLanguage(Languages.RUST)),
+      new RustaceanTrophy(this.getAcceptedCountByLanguage(Languages.RUST))
     );
     this.addTrophyAsNeeded(
-      new SwiftUserTrophy(this.getAcceptedCountByLanguage(Languages.SWIFT)),
+      new SwiftUserTrophy(this.getAcceptedCountByLanguage(Languages.SWIFT))
     );
     this.addTrophyAsNeeded(
       new VimmerTrophy(this.getAcceptedCountByLanguage(Languages.VIM))
-    )
+    );
 
     // Secret trophies.
     this.trophies.push(new AllSuperRankTrophy(this.isAllSRank));
@@ -185,7 +185,7 @@ export default class TrophyList {
 
   private getAcceptedCountByLanguage(language: SupportedLanguages): number {
     const acceptedCount = this.acceptedCountByLanguageList?.get(
-      language,
+      language
     ) as number;
 
     return acceptedCount;
@@ -215,26 +215,26 @@ export default class TrophyList {
 
   filterByHidden(): void {
     this.trophies = this.trophies.filter(
-      (trophy) => !trophy.hidden || trophy.rank !== RANK.UNKNOWN,
+      (trophy) => !trophy.hidden || trophy.rank !== RANK.UNKNOWN
     );
   }
 
   filterByTitles(titles: Array<string>): void {
     this.trophies = this.trophies.filter((trophy) =>
-      trophy.filterTitles.some((title) => titles.includes(title)),
+      trophy.filterTitles.some((title) => titles.includes(title))
     );
   }
 
   filterByRanks(ranks: Array<string>): void {
     this.trophies = this.trophies.filter((trophy) =>
-      ranks.includes(trophy.rank),
+      ranks.includes(trophy.rank)
     );
   }
 
   sortByRank(): void {
     this.trophies = this.trophies.sort(
       (a: Trophy, b: Trophy) =>
-        RANK_ORDER.indexOf(a.rank) - RANK_ORDER.indexOf(b.rank),
+        RANK_ORDER.indexOf(a.rank) - RANK_ORDER.indexOf(b.rank)
     );
   }
 }
