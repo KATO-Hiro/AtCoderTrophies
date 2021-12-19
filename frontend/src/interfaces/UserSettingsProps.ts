edc3d307af91f3fd069e8990c88866cfd9da3a0a
Kato Hiroki <k.hiro1818@gmail.com>
2021-12-19 12:13:06 +0000

    :art: Enable to change advanced options (#665)

--- frontend/src/interfaces/UserSettingsProps.ts
+++ frontend/src/interfaces/UserSettingsProps.ts
@@ -3,5 +3,6 @@ import { QueryParametersProps } from './QueryParametersProps';
 export type UserSettingsProps = {
   queryParameters: QueryParametersProps;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSwitchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onClick: () => void;
 };
