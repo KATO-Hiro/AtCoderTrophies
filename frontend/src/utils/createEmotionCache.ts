935d1c0cc5340a0b572baf51dc37430e60ea7cc5
Kato Hiroki <k.hiro1818@gmail.com>
2021-12-27 11:31:07 +0000

    :recycle: Add type (#707)

--- frontend/src/utils/createEmotionCache.ts
+++ frontend/src/utils/createEmotionCache.ts
@@ -1,7 +1,7 @@
-import createCache from '@emotion/cache';
+import createCache, { EmotionCache } from '@emotion/cache';
 
 // See:
 // https://github.com/mui-org/material-ui/blob/0620bb0c47c9aa52a863d8ccca5ce7352274bc65/examples/nextjs-with-typescript/src/utils/createEmotionCache.ts
-export default function createEmotionCache() {
+export default function createEmotionCache(): EmotionCache {
   return createCache({ key: 'css' });
 }
